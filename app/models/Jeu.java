1dcb1ab15f07c7bb593b4ae2ec589a48c7c5fc03
Clément Marziani <c.marziani@laposte.net>
2014-02-13 10:32:33 +0100

    Remplissage des fichiers java dans app/Models/

---
+++ app/models/Jeu.java
@@ -0,0 +1,9 @@
+public class Jeu{
+	static int nbJeux=4;
+	static int nbCartes=52*nbJeux;
+}
+
+public Jeu(){
+	
+}
+
---
+++ app/models/Joueur.java
@@ -0,0 +1,8 @@
+public class Joueur{
+	static int[] cartes = new int[10];
+	static string nom;
+}
+
+public Joueur(){
+
+}

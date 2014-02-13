92ba368dc39c22a5d013301849cc5fd49501223b
EtienneCh <etienne.chandelle@gmail.com>
2014-02-13 10:37:43 +0100

    Ajout methodes pour tirer carte aleatoire

--- app/models/Jeu.java
+++ app/models/Jeu.java
@@ -1,9 +1,20 @@
 public class Jeu{
 	static int nbJeux=4;
 	static int nbCartes=52*nbJeux;
-}
+
 
 public Jeu(){
 	
 }
 
+ static int randCarte(int nbCartes){
+	 Random rand = new Random();
+	 int carte = rand.nextInt(nbCartes - 1);	 
+
+	 return (carte%13)+1;
+ }
+ 
+  public static int tirerCarte(){
+	 return randCarte(nbCartes);
+ }
+}

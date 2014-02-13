83bce1a6e1590a72b65c3b0fb4edb5769ec19e82
EtienneCh <etienne.chandelle@gmail.com>
2014-02-13 11:16:00 +0100

    Ajout méthode saisir nom joueur

--- app/models/Jeu.java
+++ app/models/Jeu.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Jeu{
 	static int nbJeux=4;
 	static int nbCartes=52*nbJeux;
-
+	static ArrayList<Joueur> tab_Joueurs = new ArrayList<Joueur>();
 
 public Jeu(){
 	
@@ -18,5 +18,19 @@ public Jeu(){
  
   public static int tirerCarte(){
 	 return randCarte(nbCartes);
+ }
+ 
+ public void saisir(){
+	String nom;
+	Scanner entree= new Scanner(System.in);
+	try{ 
+	    System.out.println("nom joueur :");
+	    nom=entree.next();
+	    tab_Joueurs.add(new Joueur(nom));
+	}
+	catch(Exception e){
+	    System.err.println("Saisie invalide : recommencer");
+	    saisir();
+	}
  }
 }
--- app/models/Partie.java
+++ app/models/Partie.java
@@ -2,6 +2,6 @@ public class Partie{
 	
 	public static void main(String[] args){
 		Jeu jeu = new Jeu();
-		
+		jeu.saisir();
 	}
 }
